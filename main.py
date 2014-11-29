3f19f03625b6f39c16aab8a80b4e3a26302439b8
KeyWeeUsr <KeyWeeUsr@users.noreply.github.com>
2014-11-29 02:37:47 +0100

    v0.2

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 # -*- coding: utf-8 -*-
+
+__author__ = "KeyWeeUsr"
+__version__ = "0.2.0"
+
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.scrollview import ScrollView
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.button import Button
-from kivy.uix.label import Label
-from kivy.uix.scrollview import ScrollView
-from kivy.properties import StringProperty, BoundedNumericProperty
 from kivy.uix.modalview import ModalView
+from kivy.properties import StringProperty, BoundedNumericProperty
 
 import os
-import time
 import sys
+import time
 
 Builder.load_string('''
 <Body>:
@@ -20,12 +24,12 @@ Builder.load_string('''
         size_hint: 1,0.08
         Button:
             text: 'Add'
-            on_release: root.modalnew.open()
+            on_release: root.newword.open()
         Label:
-            text: 'Glossary v0.1\\n(c)P.B. 2014'
+            text: 'kDicts v0.2'
         Button:
-            text: 'Save'
-            on_release: root.save()
+            text: 'About'
+            on_release: root.about()
     ScrollView:
         GridLayout:
             cols: 1
@@ -39,348 +43,129 @@ Builder.load_string('''
     Label:
         text: root.word
     Label:
-        text: root.val
+        text: root.translation
+
 <NewWord>:
-    id: ModalNew
     BoxLayout:
+        spacing: 10
+        size_hint:0.5,0.7
+        pos_hint: {'center_x':0.5,'center_y':0.5}
         orientation: 'vertical'
         Label:
+            text: 'Insert a word'
+            size_hint: None,0.2
+            size: self.texture_size
+            pos_hint: {'center_x':0.5}
+        TextInput:
             id: newone
-            size_hint: 1,0.1
+            pos_hint: {'center_x':0.5}
+            size_hint: 0.8,0.2
+            size: 0,30
+            multiline: False
         Label:
+            text: 'Insert a translation'
+            size_hint: None,0.2
+            size: self.texture_size
+            pos_hint: {'center_x':0.5}
+        TextInput:
             id: newonetrans
-            size_hint: 1,0.1
-        ScreenManager:
-            id: kboard
-            Screen:
-                name: 'default'
-                BoxLayout:
-                    orientation: 'vertical'
-                    BoxLayout:
-                        Button:
-                            text: '<-'
-                            on_release: root.body.back()
-                        Button:
-                            text: '->'
-                            on_release: root.body.next()
-                    BoxLayout:
-                        Button:
-                            text: 'q'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'w'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'e'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'r'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 't'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'y'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'u'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'i'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'o'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'p'
-                            on_release: root.body.write(self.text)
-                    BoxLayout:
-                        Button:
-                            text: 'a'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 's'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'd'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'f'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'g'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'h'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'j'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'k'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'l'
-                            on_release: root.body.write(self.text)
-                    BoxLayout:
-                        Button:
-                            text: '^'
-                            on_release: root.body.shift()
-                        Button:
-                            text: 'z'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'x'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'c'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'v'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'b'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'n'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'm'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: '<<'
-                            on_release: root.body.erase()
-                    BoxLayout:
-                        Button:
-                            text: '?'
-                            on_release: root.body.change('others')
-                        Button:
-                            text: '<space>'
-                            on_release: root.body.write(' ')
-                        Button:
-                            text: 'Done'
-                            on_release: root.body.add()
-            Screen:
-                name: 'others'
-                BoxLayout:
-                    orientation: 'vertical'
-                    BoxLayout:
-                        Button:
-                            text: '<-'
-                            on_release: root.body.back()
-                        Button:
-                            text: '->'
-                            on_release: root.body.next()
-                    BoxLayout:
-                        Button:
-                            text: 'ä'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'ë'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'ö'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'ü'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'ß'
-                            on_release: root.body.write(self.text)
-                    BoxLayout:
-                        Button:
-                            text: 'Ä'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'Ë'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'Ö'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'Ü'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'á'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'é'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'í'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'ó'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'ú'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'ý'
-                            on_release: root.body.write(self.text)
-                    BoxLayout:
-                        Button:
-                            text: 'ŕ'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'ĺ'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'ř'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'ť'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'š'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'ď'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'ľ'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'ž'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'č'
-                            on_release: root.body.write(self.text)
-                    BoxLayout:
-                        Button:
-                            text: 'ň'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'Ú'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'Ř'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'Ľ'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'Š'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'Č'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'Ť'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: 'Ž'
-                            on_release: root.body.write(self.text)
-                        Button:
-                            text: '<<'
-                            on_release: root.body.erase()
-                    BoxLayout:
-                        Button:
-                            text: 'Back'
-                            on_release: root.body.change('default')
-                        Button:
-                            text: '<space>'
-                            on_release: root.body.write(' ')
-                        Button:
-                            text: 'Done'
-                            on_release: root.body.add()
+            pos_hint: {'center_x':0.5}
+            size_hint: 0.8,0.2
+            size:0,30
+            multiline: False
+        BoxLayout:
+            spacing: 20
+            size_hint: 1,0.2
+            size: 0,50
+            pos_hint: {'center_x':0.5}
+            Button:
+                text: 'Done'
+                on_release: root.body.add()
+            Button:
+                text: 'Back'
+                on_release: root.dismiss()
+                
+<AboutPopup>:
+    size_hint: 0.4,0.4
+    BoxLayout:
+        size_hint: 0.8,0.8
+        orientation: 'vertical'
+        Label:
+            text: '(C) KeyWeeUsr\\n(P.B.) 2014'
+        Button:
+            size_hint: 0.6,0.2
+            pos_hint: {'center_x':0.5}
+            on_release: root.dismiss()
+            text: 'Dismiss'
 ''')
+
+class Word(BoxLayout):
+    word = StringProperty()
+    translation = StringProperty()
 
 class NewWord(ModalView):
     pass
 
-class Word(BoxLayout):
-    val = StringProperty()
-    word = StringProperty()
+class AboutPopup(ModalView):
+    pass
     
 class Body(BoxLayout):
 
-    capital=0
     dictionary = {}
     path = os.path.dirname(os.path.abspath(__file__))+'/data'
-    selectedlabel = 'word'
-    temp = {}
     words_height = BoundedNumericProperty(0,min=0)
 
     def __init__(self, **kwargs):
         super(Body,self).__init__(**kwargs)
-        self.modalnew = NewWord()
-        self.modalnew.body = self
+        self.newword = NewWord()
+        self.newword.body = self
+        self.aboutpopup = AboutPopup()
         self.load()
 
     def load(self):
-        with open(self.path+'/gloss.txt') as f:
+        with open(self.path+'/kDicts.txt') as f:
            for line in f:
-               (key,val) = line.split()
-               self.temp[str(key)] = val
-        self.dictionary = self.temp
-        self.temp = {}
+               (word,translation) = line.split()
+               self.dictionary[str(word)] = translation
         self.ids.scrollwords.clear_widgets(children=None)
         for i in sorted(self.dictionary):
             self.words_height = self.words_height + 20
-            self.ids.scrollwords.add_widget(Word(word=str(i),val=str(self.dictionary[i])))
-
-    def save(self):
-        with open(self.path+'/gloss.txt','w') as f:
-            for i in self.dictionary:
-                f.write(str(i + ' ' + self.dictionary[i] + '\n'))
+            self.ids.scrollwords.add_widget(Word(word=str(i),translation=str(self.dictionary[i])))
 
     def backup(self):
-        filetime = str(time.ctime())
-        filetime = filetime.replace(' ','_')
-        filetime = filetime.replace(':','_')
-        with open(self.path+'/gloss' + filetime + '.txt', 'w') as f:
+        filetime = str(time.ctime()).replace(' ','_').replace(':','_')
+        with open(self.path+'/kDicts' + filetime + '.txt', 'w') as f:
             for i in self.dictionary:
                 f.write(str(i + ' ' + self.dictionary[i] + '\n'))
 
-    def next(self):
-        self.selectedlabel = 'trans'
+    def save(self):
+        with open(self.path+'/kDicts.txt','w') as f:
+            for i in self.dictionary:
+                f.write(str(i + ' ' + self.dictionary[i] + '\n'))
 
-    def back(self):
-        self.selectedlabel = 'word'
 
     def add(self):
         self.backup()
-        word = self.modalnew.ids.newone.text
-        tran = self.modalnew.ids.newonetrans.text
-        self.dictionary[word] = tran
+        word = self.newword.ids.newone.text
+        translation = self.newword.ids.newonetrans.text
+        self.dictionary[word] = translation
         self.save()
-        self.selectedlabel = 'word'
         self.load()
-        self.modalnew.ids.newone.text = ''
-        self.modalnew.ids.newonetrans.text = ''
-        self.modalnew.dismiss()
+        self.newword.dismiss()
+        self.newword.ids.newone.text = ''
+        self.newword.ids.newonetrans.text = ''
 
-    def write(self, letter):
-        if self.capital == 0:
-            if self.selectedlabel == 'word':
-                self.modalnew.ids.newone.text = self.modalnew.ids.newone.text + letter
-            elif self.selectedlabel == 'trans':
-                self.modalnew.ids.newonetrans.text = self.modalnew.ids.newonetrans.text + letter
-        elif self.capital == 1:
-            if self.selectedlabel == 'word':
-                self.modalnew.ids.newone.text = self.modalnew.ids.newone.text + letter.upper()
-            elif self.selectedlabel == 'trans':
-                self.modalnew.ids.newonetrans.text = self.modalnew.ids.newonetrans.text + letter.upper()
+    def about(self):
+        self.aboutpopup.open()
 
-    def erase(self):
-        if self.selectedlabel == 'word':
-            self.modalnew.ids.newone.text = self.modalnew.ids.newone.text[:-1]
-        elif self.selectedlabel == 'trans':
-            self.modalnew.ids.newonetrans.text = self.modalnew.ids.newonetrans.text[:-1]
+class kDicts(App):
 
-    def shift(self):
-        if self.capital == 0:
-            self.capital=1
-        elif self.capital ==1:
-            self.capital=0
+    use_kivy_settings = False
+    def open_settings(self,*largs):
+        pass
 
-    def change(self, screen):
-        self.modalnew.ids.kboard.current = screen
-
-class Glossary(App):
     def build(self):
         return Body()
 
 if __name__ == '__main__':
-    Glossary().run()
+    kDicts().run()
