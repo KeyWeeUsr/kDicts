5a8376c50593ddef1c5c6042b9d282c94f11128b
KeyWeeUsr <KeyWeeUsr@users.noreply.github.com>
2014-12-01 13:37:09 +0100

    v0.3
    Added support for in-app editing of dictionary file(swipe right to left)
    + fixes

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 
 __author__ = "KeyWeeUsr"
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.label import Label
 from kivy.uix.button import Button
+from kivy.uix.carousel import Carousel
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
@@ -26,17 +27,37 @@ Builder.load_string('''
             text: 'Add'
             on_release: root.newword.open()
         Label:
-            text: 'kDicts v0.2'
+            text: 'kDicts v0.3'
         Button:
             text: 'About'
             on_release: root.about()
-    ScrollView:
-        GridLayout:
-            cols: 1
-            id: scrollwords
-            size_hint: 1,None
-            size: 0,root.words_height
-    
+    Carousel:
+        id: slides
+        ScrollView:
+            GridLayout:
+                cols: 1
+                id: scrollwords
+                size_hint: 1,None
+                size: 0,root.words_height
+        BoxLayout:
+            orientation: 'vertical'
+            TextInput:
+                background_color: 0,0,0,1
+                foreground_color: 1,1,1,1
+                id: EditText
+                hint_text: 'Press \\'Load\\' button.'
+            BoxLayout:
+                size_hint_y: 0.1
+                Button:
+                    text: 'Save'
+                    on_release: root.edit_save()
+                Button:
+                    text: 'Load'
+                    on_release: root.edit_load()
+                Button:
+                    text: 'Help'
+                    on_release: root.edit_help()
+
 <Word>
     size_hint: 1,None
     height: 30
@@ -97,11 +118,25 @@ Builder.load_string('''
             pos_hint: {'center_x':0.5}
             on_release: root.dismiss()
             text: 'Dismiss'
+
+<EditHelp>:
+    size_hint: 0.8,None
+    size_y: self.height
+    Label:
+        text_size: self.width, None
+        size_hint_x: 0.98
+        size_hint_y: None
+        height: (self.texture_size[1]+20)
+        text: '[i][size=25]Help[/size][/i]\\nAll of the saved words are written as [word]+<space>+[translation]+<enter>. Spaces in the \\'word\\' or in the \\'translation\\' are not tolerated, use underscore instead. \\nFor example: \\"apple your_translation_for_apple\\npear your_translation_for_pear\\" An <enter> in not a neccessary after the last word with translation.'
+        markup: True
 ''')
 
 class Word(BoxLayout):
     word = StringProperty()
     translation = StringProperty()
+
+class EditHelp(ModalView):
+    pass
 
 class NewWord(ModalView):
     pass
@@ -120,17 +155,22 @@ class Body(BoxLayout):
         self.newword = NewWord()
         self.newword.body = self
         self.aboutpopup = AboutPopup()
+        self.edithelp = EditHelp()
         self.load()
 
     def load(self):
-        with open(self.path+'/kDicts.txt') as f:
+        with open(self.path+'/kDicts.txt','rU') as f:
            for line in f:
                (word,translation) = line.split()
                self.dictionary[str(word)] = translation
         self.ids.scrollwords.clear_widgets(children=None)
+        self.words_height=0
+        each_word = 0
         for i in sorted(self.dictionary):
-            self.words_height = self.words_height + 20
             self.ids.scrollwords.add_widget(Word(word=str(i),translation=str(self.dictionary[i])))
+            each_word = each_word+1
+        self.words_height = each_word*20 + each_word*10
+        self.ids.scrollwords.size = (0,int(self.words_height))
 
     def backup(self):
         filetime = str(time.ctime()).replace(' ','_').replace(':','_')
@@ -157,6 +197,20 @@ class Body(BoxLayout):
 
     def about(self):
         self.aboutpopup.open()
+
+    def edit_save(self):
+        self.backup()
+        with open(self.path+'/kDicts.txt','w') as f:
+            f.write(str(self.ids.EditText.text))
+        self.load()
+        self.ids.slides.load_previous()
+
+    def edit_load(self):
+        with open(self.path+'/kDicts.txt','rU') as f:
+            self.ids.EditText.text = f.read()
+
+    def edit_help(self):
+        self.edithelp.open()
 
 class kDicts(App):
 
